522339d2b5468ec49fd13d903482058ea3c16329
Valeria <valeriam.bonilla07@gmail.com>
2022-04-07 16:46:39 -0400

    Add project files.

---
+++ Program7/frmprogram7.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmprogram7
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.lstBox = New System.Windows.Forms.ListBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.BackColor = System.Drawing.Color.YellowGreen
+        Me.btnMultiply.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMultiply.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnMultiply.Location = New System.Drawing.Point(106, 308)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(263, 42)
+        Me.btnMultiply.TabIndex = 0
+        Me.btnMultiply.Text = "Display Multiplication Table"
+        Me.btnMultiply.UseVisualStyleBackColor = False
+        '
+        'lstBox
+        '
+        Me.lstBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstBox.FormattingEnabled = True
+        Me.lstBox.ItemHeight = 20
+        Me.lstBox.Location = New System.Drawing.Point(57, 18)
+        Me.lstBox.Name = "lstBox"
+        Me.lstBox.Size = New System.Drawing.Size(361, 284)
+        Me.lstBox.TabIndex = 1
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.YellowGreen
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnExit.Location = New System.Drawing.Point(427, 308)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(263, 42)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'frmprogram7
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lstBox)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Name = "frmprogram7"
+        Me.Text = "Multiplication Table"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents lstBox As ListBox
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Program7/frmprogram7.vb
@@ -0,0 +1,17 @@
+﻿Public Class frmprogram7
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        'Multiplication for 10. Must loop at least once. Format with seperation.
+
+
+        For i As Integer = 1 To 10
+            lstBox.Items.Add(i & "  " & i * 2 & "  " & i * 3 & "  " & i * 4 & "  " & i * 5 & "  " & i * 6 & "  " & i * 7 & "  " & i * 8 & "  " & i * 9 & "  " & i * 10)
+
+        Next
+
+
+    End Sub
+End Class
